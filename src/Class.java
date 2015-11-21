d1823997a79340bf6170e1a2e9627f65a221569a
ar7mork <ar7mork@gmail.com>
2015-11-21 15:29:48 +0100

    wszystkie allahy

--- src/Class.java
+++ src/Class.java
@@ -1,4 +1,10 @@
 
-public class Class {
-
+public class Class{
+	public static void main(String[] args) {
+		Nstatic localNstaticVariable0 = new Nstatic();
+		Nstatic localNstaticVariable1 = new Nstatic();
+		
+		localNstaticVariable0.display();
+		localNstaticVariable1.display();
+	}
 }
--- src/Nstatic.java
+++ src/Nstatic.java
@@ -1,4 +1,11 @@
 
 public class Nstatic {
-
-}
+	public int myIntegerField;
+	
+	public void display() {
+		System.out.println("My field: "+ myIntegerField);
+	}
+	public Nstatic()	{
+		myIntegerField =0;
+		}
+	}	
